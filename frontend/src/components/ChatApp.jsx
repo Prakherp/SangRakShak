740efb772786f473f45515f4f32e7e9e5a7e7566
Prakherp <79146254+Prakherp@users.noreply.github.com>
2024-07-15 12:24:53 +0530

    ADD Q/A test components

--- frontend/src/components/ChatApp.jsx
+++ frontend/src/components/ChatApp.jsx
@@ -5,7 +5,7 @@ import Sidebar from './Sidebar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophone, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
 import './ChatApp.css';
-import { checkAuthStatus } from './../utils';
+import { checkAuthStatus } from '../utils';
 import { useNavigate } from 'react-router-dom';
 
 function ChatApp({ changeIsAuthenticated, handleLogout }) {
@@ -68,6 +68,23 @@ function ChatApp({ changeIsAuthenticated, handleLogout }) {
   };
 
   const [chats] = useState(['Chat 1', 'Chat 2', 'Chat 3']);
+  const test = [{
+    user_id: "jkashdjas",
+    "chat_1": [{
+      question: "Question1",
+      answer: "Answer1 - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },{
+      question: "Question2",
+      answer: "Answer2 - It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    }],
+    "chat_2": [{
+      question: "Question1",
+      answer: "Answer1"
+    },{
+      question: "Question2",
+      answer: "Answer2"
+    }]
+  }];
 
   if (isAuthenticated === null) {
     return <div>Loading...</div>; // or some loading spinner
@@ -84,9 +101,10 @@ function ChatApp({ changeIsAuthenticated, handleLogout }) {
       </button>
       </span>
         <div className="chatbox">
-          {chatHistory.map((chat, index) => (
-            <div key={index} className={`message ${chat.user.toLowerCase()}-message`}>
-              <p>{chat.user}: {chat.message}</p>
+          {test[0]["chat_1"].map((chat, index) => (
+            <div key={index} className={`message`}>
+              <p className='question'>{chat.question}</p>
+              <p className='answer'>{chat.answer}</p>
             </div>
           ))}
         </div>
